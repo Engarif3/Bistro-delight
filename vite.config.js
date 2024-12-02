318b7324af131c5ed54667bdf88fa9e08919f398
engarif3 <arif.aust.eng@gmail.com>
2024-12-02 20:50:18 +0100

    server object added to vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',    // Allow connections from outside the container
+    port: 5173,          // Set the port to 3001 (or another port you prefer)
+  },
   plugins: [react()],
   css: {
     postcss: {
